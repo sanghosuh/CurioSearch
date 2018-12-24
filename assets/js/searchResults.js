17e10389cb22d9bfd3279f8a638be2d382333b23
Sangho Suh <sh31659@gmail.com>
2018-12-24 04:05:07 -0500

    Create searchResults.js file

---
+++ assets/js/searchResults.js
@@ -0,0 +1,56 @@
+results = {
+	abrahamLincoln: {
+		result1: {
+			title: 'title1',
+			desc: 'desc1',
+			url: 'url1'
+		},
+		result2: {
+			title: 'title2',
+			desc: 'desc2',
+			url: 'url2'
+		},		
+		result3: {
+			title: 'title3',
+			desc: 'desc3',
+			url: 'url3'
+		},		
+		result4: {
+			title: 'title4',
+			desc: 'desc4',
+			url: 'url4'
+		},		
+		result5: {
+			title: 'title5',
+			desc: 'desc5',
+			url: 'url5'
+		}					
+	},
+	WWII: {
+		result1: {
+			title: 'title1',
+			desc: 'desc1',
+			url: 'url1'
+		},
+		result2: {
+			title: 'title2',
+			desc: 'desc2',
+			url: 'url2'
+		},		
+		result3: {
+			title: 'title3',
+			desc: 'desc3',
+			url: 'url3'
+		},		
+		result4: {
+			title: 'title4',
+			desc: 'desc4',
+			url: 'url4'
+		},		
+		result5: {
+			title: 'title5',
+			desc: 'desc5',
+			url: 'url5'
+		}
+	}
+}
