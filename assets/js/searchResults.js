04c77407bf979ec1228aefa7af4277a4480ad068
Sangho Suh <sh31659@gmail.com>
2019-02-03 01:09:12 -0500

    Add results for George Washington and Donald Trump

--- assets/js/searchResults.js
+++ assets/js/searchResults.js
@@ -1,4 +1,4 @@
-generalCond1Results = [
+abrahamLincolnCond1Results = [
 	{
 		title: 'Abraham Lincoln - Wikipedia',
 		url: 'https://en.wikipedia.org/wiki/Abraham_Lincoln',
@@ -26,7 +26,7 @@ generalCond1Results = [
 	}			
 ]
 
-generalCond2Results = [
+abrahamLincolnCond2Results = [
 	{
 		title: 'Abraham Lincoln - Wikipedia',
 		url: 'https://en.wikipedia.org/wiki/Abraham_Lincoln',
@@ -54,7 +54,7 @@ generalCond2Results = [
 	}				
 ]
 
-generalCond3Results = [
+abrahamLincolnCond3Results = [
 	{
 		title: 'Abraham Lincoln - Wikipedia',
 		url: 'https://en.wikipedia.org/wiki/Abraham_Lincoln',
@@ -80,4 +80,60 @@ generalCond3Results = [
 		url: 'https://www.amazon.co.uk/Abraham-Lincoln/e/B000AP7NEQ',
 		description: 'by Abraham Lincoln and Henry R Luce Professor of the Civil War Era and Director of Civil War Era Studies Allen C Guelzo ...'
 	}				
+]
+
+georgeWashingtonCond1Results = [
+	{
+		title: 'George Washington - Wikipedia',
+		url: 'https://en.wikipedia.org/wiki/George_Washington',
+		description: 'George Washington was an American political leader, military general, statesman, and Founding Father who served as the first president of the United States ...'
+	},
+	{
+		title: 'George Washington - HISTORY',
+		url: 'https://www.history.com/topics/us-presidents/george-washington',
+		description: 'George Washington (1732-99) was commander in chief of the Continental Army during the American Revolutionary War (1775-83) and ...'
+	},	
+	{
+		title: 'Biography of George Washington · George Washington\'s Mount Vernon',
+		url: 'https://www.mountvernon.org/george-washington/biography/',
+		description: 'George Washington – first American president, commander of the Continental Army, president of the Constitutional Convention, and gentleman planter ...'
+	},		
+	{
+		title: 'George Washington · George Washington\'s Mount Vernon',
+		url: 'https://www.mountvernon.org/george-washington/',
+		description: "Little is known of George Washington's childhood, and it remains the most poorly understood part of his life. His early experiences working as a surveyor and in ..."
+	},		
+	{
+		title: 'George Washington - Life, Farewell Address & Quotes - Biography',
+		url: 'https://www.biography.com/people/george-washington-9524786',
+		description: 'Discover details about the life and career of George Washington, a military and political leader of the Continental Army in the American Revolution, and the first ...'
+	}
+]
+
+donaldTrumpCond1Results = [
+	{
+		title: 'Donald Trump - Wikipedia',
+		url: 'https://en.wikipedia.org/wiki/Donald_Trump',
+		description: 'Donald John Trump (born June 14, 1946) is the 45th and current president of the United States. Before entering politics, he was a businessman and television ...'
+	},
+	{
+		title: 'Donald J. Trump for President: Home',
+		url: 'https://www.donaldjtrump.com/',
+		description: 'Help continue our promise to Make America Great Again!'
+	},	
+	{
+		title: 'Donald Trump - CNBC - CNBC.com',
+		url: 'https://www.cnbc.com/donald-trump/',
+		description: '*Nonfarm payrolls jump by 304,000 jobs in January. WASHINGTON, Feb 1- U.S. job growth surged in January, with employers hiring the most workers in 11 ...'
+	},		
+	{
+		title: '\'All alone\' no more: Trump returns to Mar-a-Lago',
+		url: 'https://www.cnn.com/2019/02/02/politics/donald-trump-mar-a-lago-shutdown/index.html',
+		description: "He described it almost like monastic seclusion: a month-long self-exile in the White House with only armed guards as company."
+	},		
+	{
+		title: 'Donald Trump: Inside the White House With the President',
+		url: 'http://time.com/donald-trump-after-hours/',
+		description: 'President Donald Trump spends his time after hours in the White House with a mix of cable television news, Twitter and more.'
+	}
 ]
