3daad9441f18c3f5e5eec6316c58e54812b7ca90
sanghosuh <sh31659@gmail.com>
2019-02-04 09:50:54 -0500

    Modify title to question format

--- assets/js/searchResults.js
+++ assets/js/searchResults.js
@@ -400,3 +400,266 @@ level1Results = [
 	// queries
 	['president', 'programmer', 'professor']
 ]
+
+
+//
+
+level3Cond3Results = [
+	[
+		{
+			title: 'What Is His Nickname?',
+			url: 'https://en.wikipedia.org/wiki/George_Washington',
+			description: 'George Washington was an American political leader, military general, statesman, and Founding Father who served as the first president of the United States ...'
+		},
+		{
+			title: 'What Was He During the American Revolutionary War?',
+			url: 'https://www.history.com/topics/us-presidents/george-washington',
+			description: 'George Washington (1732-99) was commander in chief of the Continental Army during the American Revolutionary War (1775-83) and ...'
+		},	
+		{
+			title: 'When Did His Father Die?',
+			url: 'https://www.mountvernon.org/george-washington/biography/',
+			description: 'George Washington – first American president, commander of the Continental Army, president of the Constitutional Convention, and gentleman planter ...'
+		},		
+		{
+			title: 'What Was the Cause of His Death?',
+			url: 'https://www.mountvernon.org/george-washington/',
+			description: "Little is known of George Washington's childhood, and it remains the most poorly understood part of his life. His early experiences working as a surveyor and in ..."
+		},		
+		{
+			title: 'Who Was George Washington?',
+			url: 'https://www.biography.com/people/george-washington-9524786',
+			description: 'Discover details about the life and career of George Washington, a military and political leader of the Continental Army in the American Revolution, and the first ...'
+		}
+	],
+	[
+		{
+			title: 'What Did He Serve As From 1861 to 1865?',
+			url: 'https://en.wikipedia.org/wiki/Abraham_Lincoln',
+			description: 'What was he before serving as this of the United States from 1861 ...'
+		},
+		{
+			title: 'What Did He Have Prior to His Election As President?',
+			url: 'www.historynet.com/abraham-lincoln',
+			description: 'Facts, information and articles about the life of a man, 16th President Of the United States Abraham Lincoln Facts Born February 12, 1809, Hodgen.'
+		},	
+		{
+			title: 'On January 1, 1863, What Did He Deliver?',
+			url: 'https://www.biography.com/people/abraham-lincoln-9382540',
+			description: 'Journey through the life of him, the 16th U.S. president, on Biography.com. Learn more about his roles in the Civil War and the ...'
+		},		
+		{
+			title: 'What is Marfan Syndrome?',
+			url: 'http://time.com/5135506/abraham-lincoln-marfan-syndrome/',
+			description: "What did he have that would explain why he was so tall? Here's what that syndrome is and its symptoms."
+		},		
+		{
+			title: 'What Did He Bring About?',
+			url: 'https://www.britannica.com/biography/Abraham-Lincoln',
+			description: 'by Abraham Lincoln and Henry R Luce Professor of the Civil War Era and Director of Civil War Era Studies Allen C Guelzo ...'
+		}				
+	],
+	[
+		{
+			title: 'What Was He Before Entering Politics?',
+			url: 'https://en.wikipedia.org/wiki/Donald_Trump',
+			description: 'Donald John Trump (born June 14, 1946) is the 45th and current president of the United States. Before entering politics, he was a businessman and television ...'
+		},
+		{
+			title: 'What Did the American People Deliver on November 8, 2016?',
+			url: 'https://www.donaldjtrump.com/',
+			description: 'Help continue our promise to Make America Great Again!'
+		},	
+		{
+			title: 'He Is the Nation\'s __ President',
+			url: 'https://www.cnbc.com/donald-trump/',
+			description: '*Nonfarm payrolls jump by 304,000 jobs in January. WASHINGTON, Feb 1- U.S. job growth surged in January, with employers hiring the most workers in 11 ...'
+		},		
+		{
+			title: 'What Did He Describe It As?',
+			url: 'https://www.cnn.com/2019/02/02/politics/donald-trump-mar-a-lago-shutdown/index.html',
+			description: "He described it almost like monastic seclusion: a month-long self-exile in the White House with only armed guards as company."
+		},		
+		{
+			title: 'What Did He Write In A Tweet on Sunday Evening?',
+			url: 'https://www.cnn.com/2019/02/04/politics/trump-state-of-the-union-unity/index.html',
+			description: 'President Donald Trump is carving new divides on immigration ahead of his State of the Union address that likely offer a more reliable guide to ...'
+		}
+	],
+	// queries
+	['george washington', 'abraham lincoln', 'donald trump']
+]
+
+level2Cond3Results = [
+	[
+		{
+			title: 'Which Branch of the Federal Government Does the President Direct?',
+			url: 'https://en.wikipedia.org/wiki/President_of_the_United_States',
+			description: 'Every president since John Adams (in 1800) has lived there. At various times in U.S. history, it has been known as the "President\'s Palace," the "President\'s House," ...'
+		},
+		{
+			title: 'Who Was Elected to Be the 45th President of the United States?',
+			url: 'https://simple.wikipedia.org/wiki/President_of_the_United_States',
+			description: 'The first President (George Washington) took office in 1789. The President serves as chief executive and is in charge of the ...'
+		},	
+		{
+			title: 'List of Presidents of the United States - Simple English Wikipedia, the ...',
+			url: 'https://simple.wikipedia.org/wiki/List_of_Presidents_of_the_United_States',
+			description: 'This list includes all people who have been sworn as President of the United ...... "Donald Trump – Republican Party – 45thPresident – American Presidents". ...'
+		},		
+		{
+			title: 'Presidents',
+			url: 'https://www.whitehouse.gov/about-the-white-house/presidents/',
+			description: "Which President served as a lieutenant colonel in the Spanish-American war? Who was the first Democrat elected after the Civil War? Who introduced Social ..."
+		},		
+		{
+			title: 'U.S. President - Biography',
+			url: 'https://www.biography.com/people/groups/political-leaders-us-presidents',
+			description: 'U.S. President. The first U.S. president, former military leader George Washington, took his oath of office on April 30, 1789, on the balcony of Federal Hall.'
+		}
+	],
+	[
+		{
+			title: 'Who Is the Prime Minister of Canada?',
+			url: 'https://en.wikipedia.org/wiki/Prime_Minister_of_Canada',
+			description: 'The current, and 23rd, Prime Minister of Canada is the Liberal Party\'s Justin Trudeau, following the 2015 Canadian federal election.'
+		},
+		{
+			title: 'Who Appoints the Prime Minister?',
+			url: 'https://en.wikipedia.org/wiki/List_of_Prime_Ministers_of_Canada',
+			description: 'The Prime Minister of Canada is an official who serves as the primary minister of the Crown, ..... '
+		},	
+		{
+			title: 'Who Is A Canadian Politician Serving As the 23rd Prime Minister of Canada?',
+			url: 'https://en.wikipedia.org/wiki/Justin_Trudeau',
+			description: 'Justin Trudeau. Justin Pierre James Trudeau PC MP (/ˈtrʊdoʊ/; French: [ʒystɛ̃ tʁydo]; born December 25, 1971) is a Canadian politician serving as the 23rd prime minister of Canada since ...'
+		},		
+		{
+			title: 'List of prime ministers of Canada | Britannica.com',
+			url: 'https://www.britannica.com/topic/list-of-prime-ministers-of-Canada-1800352',
+			description: "List of prime ministers of Canada: Though the titular head of Canada is the reigning monarch of the United Kingdom (represented locally by a governor-general), ..."
+		},		
+		{
+			title: 'Prime minister vs. president: what’s the difference?',
+			url: 'https://www.cbc.ca/kidscbc2/the-feed/prime-minister-vs.-president-whats-the-difference',
+			description: 'Here in Canada, we have a prime minister, but in the United States, their leader is a president. While both jobs are positions of power, there are ...'
+		}
+	],
+	[
+		{
+			title: 'Which Branch Was the President of Russia Head of?',
+			url: 'https://en.wikipedia.org/wiki/President_of_Russia',
+			description: 'The centers will be built for each former president of Russia. The first such center dedicated to Boris Yeltsin was opened in 2015 in Yekaterinburg.'
+		},
+		{
+			title: 'President of Russia (@KremlinRussia_E) · Twitter',
+			url: 'https://twitter.com/KremlinRussia_E?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
+			description: 'Meeting with Sergei Lavrov and Sergei Shoigu: update on the Treaty on the Elimination of Intermediate-Range and Shorter-Range Missiles http://bit.ly/2DQOWQZ'
+		},	
+		{
+			title: 'Who Is Serving As President of Russia Since 2012?',
+			url: 'https://en.wikipedia.org/wiki/Vladimir_Putin',
+			description: 'Vladimir Vladimirovich Putin is a Russian politician and former intelligence officer serving as President of ...'
+		},		
+		{
+			title: 'List of Presidents of Russia - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/List_of_Presidents_of_Russia',
+			description: "Boris Yeltsin came to power with a wave of high expectations. On 12 June 1991 he was elected president of the Russian Soviet Federative Socialist Republic with ..."
+		},		
+		{
+			title: 'President of Russia',
+			url: 'http://en.kremlin.ru/',
+			description: 'Gathering in honour of the 10th anniversary of the Russian Orthodox Church Local Council and the ... Presidential Executive OfficeLatest news and events ...'
+		}
+	],
+	// queries
+	['us president', 'canada president', 'russia president']
+]
+
+level1Cond3Results = [
+	[
+		{
+			title: 'President of the United States - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/President_of_the_United_States',
+			description: 'The President of the United States (POTUS) is the head of state and head of government of the United States of America. The president directs the executive branch ...'
+		},
+		{
+			title: 'President - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/President',
+			description: 'The president is a common title for the head of state in most republics. In politics, president is a title given to leaders of republican states.'
+		},	
+		{
+			title: 'List of Presidents of the United States - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States',
+			description: 'The President of the United States is the head of state and head of government of the United States, indirectly elected to a four-year term by the people through ...'
+		},		
+		{
+			title: 'PRESIDENT | meaning in the Cambridge English Dictionary',
+			url: 'https://dictionary.cambridge.org/dictionary/english/president',
+			description: "President Kennedy. the president of France. [ as form of address ] Thank you, Mr/Madam President. Boris Yeltsin was Russia's first democratically elected president."
+		},		
+		{
+			title: 'Presidents | The White House',
+			url: 'https://www.whitehouse.gov/about-the-white-house/presidents/',
+			description: 'Which President served as a lieutenant colonel in the Spanish-American war? Who was the first Democrat elected after the Civil War? Who introduced Social ...'
+		}
+	],
+	[
+		{
+			title: 'Programmer - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/Programmer',
+			description: 'A programmer, developer, dev, coder, or software engineer is a person who creates computer software.'
+		},
+		{
+			title: 'Computer Programmer Skills You Need to Succeed',
+			url: 'https://www.thebalancecareers.com/computer-programmer-job-description-salary-and-skills-2061823',
+			description: 'Overall, it is the responsibility of the computer programmer to write code and manipulate it into a language that the computer can understand and follow.'
+		},	
+		{
+			title: 'Computer Programmer | ComputerScience.org',
+			url: 'https://www.computerscience.org/careers/computer-programmer/',
+			description: 'Learn how to become a Computer Programmer. Research career opportunities, average salaries, key skills and sample job descriptions.'
+		},		
+		{
+			title: 'What is a Programmer? - Definition from Techopedia',
+			url: 'https://www.techopedia.com/definition/4813/programmer',
+			description: "Most programmers have a broad computing and coding background across multiple programming languages and platforms, including Structured Query ..."
+		},		
+		{
+			title: 'Programmers Guide - How to Become a Better Programmer - Due',
+			url: 'https://due.com/programmer/',
+			description: 'In this programmer guide you will find everything you need to know to be a better programmer. We teach you how to become a better programmer.'
+		}
+	],
+	[
+		{
+			title: 'Professor - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/Professor',
+			description: 'Professor (commonly abbreviated as Prof.) is an academic rank at universities and other post-secondary education and research institutions in most countries.'
+		},
+		{
+			title: 'Professor (highest academic rank) - Wikipedia',
+			url: 'https://en.wikipedia.org/wiki/Professor_(highest_academic_rank)',
+			description: 'Professor (informally also known as full professor) is the highest academic rank at universities and other institutions of higher education in parts of the world.'
+		},	
+		{
+			title: 'Professor - Simple English Wikipedia, the free encyclopedia',
+			url: 'https://simple.wikipedia.org/wiki/Professor',
+			description: 'Professor (commonly abbreviated as prof.) is an academic rank at most universities and colleges. The word professor comes from Latin.'
+		},		
+		{
+			title: 'Professor Live - YouTube',
+			url: 'https://www.youtube.com/user/Professorlive',
+			description: "The Professor is a world renowned basketball entertainer. For bookings: GloballyProfessor@gmail.com Instagram: https://www.instagram.com/globalhooper ..."
+		},		
+		{
+			title: 'Professor | Definition of Professor by Merriam-Webster',
+			url: 'https://www.merriam-webster.com/dictionary/professor',
+			description: 'Professor definition is - one that professes, avows, or declares. How to use professor in a sentence.'
+		}
+	],
+	// queries
+	['president', 'programmer', 'professor']
+]
+
+
